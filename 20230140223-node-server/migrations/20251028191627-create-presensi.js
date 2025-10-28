ebc2acca8adb9bcfe1428dcaaddfcc872d82b467
Irfansyah001 <syahirfan545@gmail.com>
2025-10-29 03:18:16 +0700

    Update migration to add allowNull constraints for userId, nama, checkIn, and checkOut fields in Presensi table

--- 20230140223-node-server/migrations/20251028191627-create-presensi.js
+++ 20230140223-node-server/migrations/20251028191627-create-presensi.js
@@ -10,7 +10,20 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       userId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      nama: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      checkIn: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      checkOut: {
+        type: Sequelize.DATE,
+        allowNull: true, // checkOut can be null initially
       },
       createdAt: {
         allowNull: false,
