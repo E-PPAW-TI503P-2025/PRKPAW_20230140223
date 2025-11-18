58f3ed9e53a2947ee2fab5913acc5a7ac40f8cb3
Irfansyah001 <syahirfan545@gmail.com>
2025-11-18 18:18:30 +0700

    Add DashboardPage component with welcome message and access note

---
+++ 20230140223-react/src/components/DashboardPage.js
@@ -0,0 +1,13 @@
+// src/components/DashboardPage.js
+import React from 'react';
+
+function DashboardPage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 80 }}>
+      <h1>Selamat Datang di Dashboard</h1>
+      <p>Halaman ini nantinya hanya bisa diakses setelah login.</p>
+    </div>
+  );
+}
+
+export default DashboardPage;
